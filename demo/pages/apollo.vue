4e47fc190e932c1d4d7acd80f67c4da7aafde0f3
Austin Story <lonnieastory@gmail.com>
2019-08-16 22:55:03 -0500

    Add form input locally that we can use to set the messsage of gql

--- demo/pages/apollo.vue
+++ demo/pages/apollo.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <span>Message {{ message }}</span>
-    <br />
-    <button @click="updateMessage">Change Message to Bar</button>
-    <br />
-    <button @click="getMessage">Get Message Value</button>
+    <div>Apollo Message "{{ message }}"</div>
+    <button @click="setApolloMessage">Update Apollo with Input</button>
+    <button @click="getApolloMessage">Get Message From Apollo</button>
+    <label>
+      Local Message
+      <input v-model="localMessage" />
+    </label>
   </div>
 </template>
 
@@ -15,18 +17,20 @@
   export default {
     setup(props, context) {
       const message = value('');
+      const localMessage = value('');
 
       const getMessageGql = gql`{ message }`;
 
-      console.log(getMessageGql)
+      console.log(getMessageGql);
       console.log(context.root.$apollo);
 
       // https://www.apollographql.com/docs/link/links/state/#writequery-and-readquery
-      function updateMessage() {
-        context.root.$apollo.writeData({ data: { message: 'bar' } });
+      function setApolloMessage() {
+        context.root.$apollo.writeData({ data: { message: localMessage.value } });
+        getApolloMessage();
       }
 
-      function getMessage() {
+      function getApolloMessage() {
         context.root.$apollo.query({
           query: getMessageGql,
         }).then(data => {
@@ -34,13 +38,41 @@
         });
       }
 
-      onMounted(getMessage);
+      // https://www.apollographql.com/docs/react/api/apollo-client/#ApolloClient.watchQuery
+      // https://www.apollographql.com/docs/react/api/apollo-client/#observablequery-functions
+      function subscribeToQuery() {
+        const observableQuery = context.root.$apollo.watchQuery({
+          query: getMessageGql
+        });
+
+        observableQuery.subscribe()
+      }
+
+      onMounted(getApolloMessage);
       // expose bindings on render context
       return {
         message,
-        updateMessage,
-        getMessage,
+        localMessage,
+        setApolloMessage,
+        getApolloMessage,
       };
     },
   };
 </script>
+
+<style>
+  button {
+    display: block;
+    background-color: #00C48D;
+    color: white;
+    height: 50px;
+    border-radius: 5px;
+    font-size: 20px;
+    margin-top: 10px;
+  }
+  input {
+    display: block;
+    height: 50px;
+    margin: 10px;
+  }
+</style>
